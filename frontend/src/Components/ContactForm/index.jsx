a8498b661ab9cb3267a812342217e51534792828
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-08-06 14:05:01 +0300

    Adjusted inputs layout

--- frontend/src/Components/ContactForm/index.jsx
+++ frontend/src/Components/ContactForm/index.jsx
@@ -42,22 +42,24 @@ const ContactForm = () => {
     }
 
     return (
-        <div className='add'>
+        <div className='input-group'>
             <h2>Add a New Contact</h2>
             <form onSubmit ={handleSubmit}>
-                <label>Contact Name:</label>
-                <input type="text" required value={ name } onChange={(e) => setName(e.target.value)}/>
+                <label className= "input-group__label">Contact Name:</label>
+                <input type="text" className= "input-group__input" required value={ name } onChange={(e) => setName(e.target.value)}/>
 
-                <label>Phone Number: </label>
-                <input type="tel" required value={ phone }  onChange={(e) => setPhone(e.target.value)}/>
+                <label className= "input-group__label">Phone Number: </label>
+                <input type="tel" className= "input-group__input" required value={ phone }  onChange={(e) => setPhone(e.target.value)}/>
 
-                <label>Latitude: </label>
-                <input type="text" required value={ latitude }  onChange={(e) => setLatitude(e.target.value)}/>
+                <label className= "input-group__label">Latitude: </label>
+                <input type="text" className= "input-group__input" required value={ latitude }  onChange={(e) => setLatitude(e.target.value)}/>
 
-                <label>Longitude: </label>
-                <input type="text" required value={ longitude } onChange={(e) => setLongitude(e.target.value)}/>
+                <label className= "input-group__label">Longitude: </label>
+                <input type="text" className= "input-group__input" required value={ longitude } onChange={(e) => setLongitude(e.target.value)}/>
 
-                <button onClick={navigateHome}>Add Contact</button>
+                <div className='button-container'>
+                    <button onClick={navigateHome}>Save Contact</button>
+                </div>
             </form>
             
         </div>
